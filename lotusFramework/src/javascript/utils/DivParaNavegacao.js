61f3c0d5483ab351e8e114a3526151cd0316bb24
Thiago Alexandre Guerra <163882581+ThiagoAlexandreGuerra@users.noreply.github.com>
2026-05-17 12:17:35 -0300

    Update DivParaNavegacao.js

--- lotusFramework/src/javascript/utils/DivParaNavegacao.js
+++ lotusFramework/src/javascript/utils/DivParaNavegacao.js
@@ -8,20 +8,43 @@ export default class DivParaNavegacao {
     }
 
     get getClassList() {
-        return this._classList;
+        return [...this.elemento.classList];
     }
 
-    _chamarProximaClasseDoLayout(value){
+    _chamarProximaClasseDoLayout(value) {
 
-        this.elemento.classList.remove(this._classList);
+        if (this._classLayoutAtual) {
+            this.elemento.classList.remove(this._classLayoutAtual);
+        }
 
-        this._classList = value;
-
+        this._classLayoutAtual = value;
         this.elemento.classList.add(value);
     }
-    
+
+    adicionarClasse(...classes) {
+        this.elemento.classList.add(...classes);
+    }
+
+    removerClasse(...classes) {
+        this.elemento.classList.remove(...classes);
+    }
+
+    alternarClasse(classe) {
+        this.elemento.classList.toggle(classe);
+    }
+
+    contemClasse(classe) {
+        return this.elemento.classList.contains(classe);
+    }
+
     renderizarDivParaNavegacao() {
-        this.elemento = renderizarElementos("div",{},{},true);
-        this.elemento.className = this._classList;
+        this.elemento = renderizarElementos("div", {}, {}, true);
+        this.elemento.classList.add(this._classLayoutAtual);
+    }
+
+    verificarClassesNoDOM(...classes) {
+        return classes
+            .filter(classe => document.querySelector(`.${classe}`))
+            .join(" ");
     }
 }
